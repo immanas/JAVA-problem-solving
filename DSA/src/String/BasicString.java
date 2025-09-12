b1df3154f0ec6cf782e6ffc7ea98504baa8f4933
Manas Gantait <162159647+immanas@users.noreply.github.com>
2025-09-12 23:25:56 +0530

    Add files via upload
    added some string problems.

---
+++ DSA/src/String/BasicString.java
@@ -0,0 +1,12 @@
+package String;
+import java.util.*;
+
+public class BasicString {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter A string :");
+        String str = sc.nextLine();
+        int n = str.length(); //To find the Length
+        System.out.println("THE output :"+n);
+    }
+}
---
+++ DSA/src/String/Function.java
@@ -0,0 +1,14 @@
+package String;
+import java.util.*;
+public class Function {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a String value :");
+        String str = "manas" ;
+        char ch = str.charAt(3);
+        int mg = str.indexOf('a');
+        System.out.println(ch);
+        System.out.println(mg);
+
+    }
+}
